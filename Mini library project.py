e4a89c4ee17b99ee0edbfeb664719bb1d8681532
Samarth170102 <68531955+Samarth170102@users.noreply.github.com>
2021-03-22 19:04:16 +0530

    Fix some errors

--- Mini library project.py
+++ Mini library project.py
@@ -18,21 +18,43 @@ class Library:
     def add_book(cls, name):
         """Function to add a new book to the library"""
 
-        cls.books.append(name)
+        if name in cls.books:
+            print("\nSorry cannot add this book\nThis book is already available in library")
+
+        elif name in cls.lend_books:
+            print("\nSorry cannot add this book\nThis book is already in lended list")
+
+        else:
+            cls.books.append(name)
 
     @classmethod
     def lend_book(cls, name):
         """Function to lend the book from the library"""
 
-        cls.lend_books.append(name)
-        cls.books.remove(name)
+        if name not in cls.books and name not in cls.lend_books:
+            print("\nSorry!this book is not available in this library")
+        
+        elif name in cls.lend_books:
+            print("\nSorry,Cannot provide this book because\nThis book is lended by someone else")
+
+        else:
+            cls.lend_books.append(name)
+            cls.books.remove(name)
 
     @classmethod
     def return_book(cls, value, name):
         """Function to return the book which was lend from the library"""
 
         if name in cls.books:
-            print("\nThe book already exist in library\n")
+            print("\nThe book already exists in library\n")
+
+        elif name not in cls.lend_books:
+            print("\nThis book is not belongs to this library")
+
+            ask2 = input("Do you want to add this book to this library?\n")
+            if ask2 == 'yes':
+                cls.books.insert(value, name)
+
         else:
             cls.lend_books.remove(name)
             cls.books.insert(value, name)
@@ -54,13 +76,11 @@ if __name__ == '__main__':
     name = str
     ask = str
 
-    name = Library()
-
     while True:
 
         ask = input("\nWhat you want to do?\n\n"
-                    'Display the books\nAdd the book\nLend the book\nReturn the lend book'
-                    '\nSee the lend books\nRemove from the program\n\n')
+                    'Display the books\nAdd the book\nLend the book\nReturn the lended book'
+                    '\nDisplay the lended books\nRemove from the program\n\n')
 
         if ask == 'Display the books':
 
@@ -77,13 +97,14 @@ if __name__ == '__main__':
             name = input("Which book you want to lend: ")
             Library.lend_book(name)
 
-        elif ask == 'Return the lend book':
+        elif ask == 'Return the lended book':
 
             name = input("Which book you want to return?: ")
-            value = int(input("Where you want to put that book?: "))
+            Library.display()
+            value = int(input("At which number you want to put that book?: "))
             Library.return_book(value - 1, name)
 
-        elif ask == 'See the lend books':
+        elif ask == 'Display the lended books':
             Library.display_lend_books()
             
         elif ask == 'Remove from the program':
